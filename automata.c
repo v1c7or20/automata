f40e625a37d21ce3331e4bf7fd2de6ed3456194c
victor <victor.pena@utec.edu.oe>
2020-07-05 20:15:45 -0500

    avance

--- automata.c
+++ automata.c
@@ -1,58 +1,110 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
-struct automata
+typedef struct 
 {
     char * name;
     bool is_end;
     int number_state;
-    struct automata * next_states;
-};
+    struct automata ** next_states;
+} automata;
 
-struct automata construct(char * nombre, bool is_end)
+typedef struct 
 {
-    struct automata constructed;
-    strncpy(constructed.name, nombre, strlen(nombre));
-    constructed.is_end = is_end;
-    constructed.next_states = NULL;
-    return constructed;
+    automata * actual_automata;
+    struct list * next_node;
+} list;
+
+void construct_node(automata * to_construct, char * nombre, bool is_end)
+{
+    to_construct = (automata *)malloc(sizeof( automata));
+    strncpy(to_construct->name, nombre, strlen(nombre));
+    to_construct->is_end = is_end;
+    to_construct->next_states = NULL;
 }
 
-void add_state(char * state_from, char * state_to, struct list list_of_states)
+void look_for_state(list * list_of_states, char * name_of_state)
 {
-    struct list * node_from = & list_of_states;
+    while (strcmp(list_of_states->actual_automata->name, name_of_state) != 0)
+    {
+        if (list_of_states->next_node != NULL)
+        {
+            list_of_states = list_of_states->next_node;
+        }
+        else
+        {
+            printf("The state: %s does not exist", name_of_state);
+            return;
+        }
+    }
+}
+
+void create_first_next_states(automata * node_from, automata * node_to)
+{
+    *(node_from->next_states) = (automata*)malloc(sizeof(automata));
+    node_from->next_states[0] = node_to;
+    node_from->number_state += 1;
+}
+
+void add_state(char * state_from, char * state_to, list * list_of_states)
+{
+    list * node_from =  list_of_states;
     look_for_state(node_from, state_from);
-    struct list * node_to = & list_of_states;
+    list * node_to = list_of_states;
     look_for_state(node_to, state_to);
-    if (node_from->actual_automata->next_states == NULL)
+    if (*(node_from->actual_automata->next_states) == NULL)
     {
         create_first_next_states(node_from->actual_automata, node_to->actual_automata);
     }
     else
     {
-        struct automata * next_states_to_copy = node_from->actual_automata->next_states;
+        automata ** next_states_to_copy = node_from->actual_automata->next_states;
         node_from->actual_automata->next_states = NULL;
         node_from->actual_automata->number_state += 1;
-        node_from->actual_automata->next_states = (struct automata*)malloc(node_from->actual_automata->number_state * sizeof(struct automata));
+        *(node_from->actual_automata->next_states) = (automata*)malloc(node_from->actual_automata->number_state * sizeof(automata ));
         for (int i = 0; i < node_from->actual_automata->number_state -1; i++)
         {
-            
+            node_from->actual_automata->next_states[i] = next_states_to_copy[i];
         }
-        
+        node_from->actual_automata->next_states[node_from->actual_automata->number_state] = node_to->actual_automata;
     }
     
 }
 
-void create_first_next_states(struct automata * node_from, struct automata * node_to)
-{
-    node_from->next_states = (struct automata*)malloc(sizeof(struct automata));
-    node_from->next_states = node_to;
-    node_from->number_state += 1;
-}
-
-
-
-struct automata * next_state(char * state_nombre, struct automata * current_state)
+automata * next_state(char * state_nombre, automata * current_state)
 {
 
 }
+
+
+list create_list(char * nombre_nodo)
+{
+    //create node of automata
+    automata * nodo_automata = (automata*)malloc(sizeof(automata));
+    construct_node(nodo_automata, nombre_nodo, true);
+    //create start of list
+    list list_of_nodes;
+    list_of_nodes.next_node = NULL;
+    list_of_nodes.actual_automata = nodo_automata;
+    return list_of_nodes;
+}
+
+void add_node(list nodo_inicial ,char * nombre_nodo)
+{
+    list *nodo_to_insert = &nodo_inicial;
+    //look for end
+    while (nodo_to_insert->next_node != NULL)
+    {
+        nodo_to_insert = nodo_to_insert->next_node;
+    }
+    //create new node
+    nodo_to_insert->actual_automata->is_end = false;
+    nodo_to_insert->next_node = (list*)malloc(sizeof(list));
+    //pass to the new node
+    nodo_to_insert = nodo_to_insert->next_node;
+    nodo_to_insert->next_node = NULL;
+    construct_node(nodo_to_insert->actual_automata, nombre_nodo, true);
+}
+
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include "list.c"
+
+int main()
+{
+    struct automata * nodo_automata;
+    construct_node(nodo_automata, "nuevo", true);
+    return true;
+}
